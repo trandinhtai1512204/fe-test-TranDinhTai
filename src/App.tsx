ff30cc1719c3618143346cbec313b4ea17fc0159
trandinhtai1512204 <taitran1524@gmail.com>
2026-05-09 12:34:00 +0700

    init project, cài đặt các thư viện cần thiết

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './index.css'
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-600">TaskBoard</h1>
+    </div>
+  )
+}
